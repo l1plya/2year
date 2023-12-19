78b807a587bc5512f8fea98820b8e50a4819c0e0
l1plya <125365167+l1plya@users.noreply.github.com>
2023-12-19 13:08:28 +0300

    Delete routers directory

--- routers/router.js
+++
@@ -1,9 +0,0 @@
-const Router = require('express');
-const router = new Router();
-const controller = require('../controllers/Controller');
-
-router.get('/', controller.indexPage);
-router.get('/feedback', controller.FeedbackPage);
-router.get('/contact', controller.ContactPage);
-
-module.exports = router;
